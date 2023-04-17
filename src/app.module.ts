6e6f9e66f8593ed544a3867e240c641d7b94b55a
Soul_Solomon <sekamattesoul@gmail.com>
2023-04-17 17:58:29 +0300

    created author and categories resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuotesModule } from './quotes/quotes.module';
+import { AuthorsModule } from './authors/authors.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [QuotesModule],
+  imports: [QuotesModule, AuthorsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
